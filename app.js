b9fd9cfd47306393ceaa8a86e1a348e241c3cd0e
Jdave-sys <jizerdave26@gmail.com>
2026-04-26 20:20:56 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-document.getElementById("postBtn").addEventListener("click", function() {
-    const input = document.getElementById("messageInput");
-    const output = document.getElementById("outputBox");
-    output.innerText = input.value || "No message entered.";
-    input.value = "";
-});
+function postAnnouncement() {
+    let message = document.getElementById("messageInput").value;
+    document.getElementById("outputBox").innerText = message;
+}
